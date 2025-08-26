00aaba01064e1029363cf2068de241fd616b95a8
Gian Peña <flaconumero2@gmail.com>
2025-08-26 00:19:55 -0400

    install dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
-import path from 'path'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const keyPath = process.env.SSL_KEY_PATH;
 const certPath = process.env.SSL_CERT_PATH;
