02e73556fac230af6ff05ba8bf6688e51aa98a8f
Gian Peña <flaconumero2@gmail.com>
2025-08-25 23:59:07 -0400

    generalize these

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
+const keyPath = process.env.SSL_KEY_PATH;
+const certPath = process.env.SSL_CERT_PATH;
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 443,
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'private-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certificate.pem')),
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
     }
   }
 })
