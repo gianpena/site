46a5bcf9d0571ec770fa7e3df10af7565b62b2d2
Gian Peña <36643266+gianpena@users.noreply.github.com>
2025-08-26 00:44:55 -0400

    Https stuff (#1)
    * ignore ssl stuff
    
    * update vite config
    
    * generalize these
    
    * ignore .env everywhere
    
    * install dotenv
    
    * set host to true
    
    * udpate site name + favicon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+dotenv.config();
+
+const keyPath = process.env.SSL_KEY_PATH;
+const certPath = process.env.SSL_CERT_PATH;
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 443,
+    host: true,
+    https: {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    }
+  }
 })
