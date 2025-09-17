76bd62c40923185c01f04d4802e104f9ce17acef
Gian Peña <flaconumero2@gmail.com>
2025-09-17 15:23:38 -0400

    Add this style to project section top level div Since the cards now have seemingly identical styling but are behaving differently on mobile, I suspect this may be the issue. Can't test until I get home though.

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,7 +4,7 @@ import { ProjectCard } from '../ProjectCard';
 
 export function Projects() {
     return (
-        <div className="center-content">
+        <div className="center-content" style={{ textAlign: 'center' }}>
             <ProjectCard title="Bedstats" description="A discord bot made to track Hypixel bedwars statistics." link="https://www.bedstats.net/"/>
             <ProjectCard title="This Website!" description="Made with React, hosted by yours truly." link="https://github.com/gianpena/site" />
             <div className="general-site-font" style={{color: '#ffffff'}}>
