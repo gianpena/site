9e8a8e4980600d2564d253d7e47488552b58f188
Gian Peña <flaconumero2@gmail.com>
2025-12-19 19:56:57 -0500

    modify App.jsx to reflect changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,10 @@ import { useState, useEffect } from "react"
 import { UsernameContext } from "./UsernameContext.js"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Navbar } from "./Navbar"
-import { AboutMe } from "./sections/about/AboutMe"
+import { HomePage } from "./HomePage"
 import { Projects } from "./sections/projects/Projects"
 import { SpeedTyping } from "./sections/speedtyping/SpeedTyping"
 import "./sections/App.css"
-
-const LandingPage = () => {
-  const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    let id = setTimeout(() => {
-      setShow(true);
-    }, 500);
-
-    return () => clearTimeout(id);
-  }, []);
-
-  return (
-    <div
-      className="center-content"
-      style={{
-        color: "#ffffff"
-      }}
-    >
-      <h1 className="general-site-font italic fade-in">
-        “You can lead a crafter to diamonds, but you can’t make him mine.”
-      </h1>
-      <h2 className={`general-site-font ${show ? "fade-in" : "invisible"}`}>Things will eventually go here.</h2>
-    </div>
-  )
-}
 
 function App() {
   const [discordUsername, setDiscordUsername] = useState("")
@@ -51,12 +25,10 @@ function App() {
     <UsernameContext value={discordUsername}>
       <BrowserRouter>
         <Navbar />
-
         <Routes>
           <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<AboutMe />} />
           <Route path="/speedtyping" element={<SpeedTyping />} />
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </UsernameContext>
