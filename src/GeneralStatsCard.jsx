0b7d3a7e0d52f86eaeb7045d780d480ec90a4edf
Gian Peña <36643266+gianpena@users.noreply.github.com>
2025-12-28 00:55:47 -0500

    Statscard timeout hotfix (#27)
    * add ten second timeout
    
    * add semicolons for consistency

--- src/GeneralStatsCard.jsx
+++ src/GeneralStatsCard.jsx
@@ -1,33 +1,47 @@
 import { useRef, useState, useEffect } from "react"
 export function GeneralStatsCard({ link, baseLoadingMessage }) {
-  const statsCardMessage = useRef("Loading LeetCode stats card...")
-  const [periods, setPeriods] = useState(2) // zero-based with +1 offset later
-  const [imageResolved, setImageResolved] = useState(false)
-  const [error, setError] = useState("")
-  const intervalId = useRef(-1)
+  const statsCardMessage = useRef("Loading LeetCode stats card...");
+  const [periods, setPeriods] = useState(2); // zero-based with +1 offset later
+  const [imageResolved, setImageResolved] = useState(false);
+  const [error, setError] = useState("");
+  const intervalId = useRef(-1);
+  const imageResolvedRef = useRef(false);
 
   function cleanup() {
     if (intervalId.current !== -1) {
-      clearInterval(intervalId.current)
-      intervalId.current = -1
+      clearInterval(intervalId.current);
+      intervalId.current = -1;
     }
   }
 
   useEffect(() => {
     intervalId.current = setInterval(() => {
       setPeriods(p => {
-        p = (p + 1) % 3
-        statsCardMessage.current = `${baseLoadingMessage}${".".repeat(p + 1)}`
-        return p
+        p = (p + 1) % 3;
+        statsCardMessage.current = `${baseLoadingMessage}${".".repeat(p + 1)}`;
+        return p;
       })
-    }, 250)
+    }, 250);
 
-    return cleanup
+    const timeoutId = setTimeout(() => {
+      if (!imageResolvedRef.current) {
+        cleanup();
+        setError(e => {
+          statsCardMessage.current = "";
+          return "Something went wrong loading the card. Try again later!";
+        });
+      }
+    }, 10000);
+
+    return () => {
+      cleanup();
+      clearTimeout(timeoutId);
+    };
   }, [])
 
   return (
     <div>
-      {!imageResolved && (
+      {(!imageResolved || error) && (
         <div
           className="general-site-font"
           style={{
@@ -42,21 +56,26 @@ export function GeneralStatsCard({ link, baseLoadingMessage }) {
           {statsCardMessage.current || error}
         </div>
       )}
-      <div align="center" style={{ marginTop: "50px" }}>
-        <img
-          src={link}
-          alt=""
-          onLoad={() => {
-            cleanup()
-            setImageResolved(true)
-          }}
-          onError={() => {
-            cleanup()
-            statsCardMessage.current = ""
-            setError("Something went wrong loading the card. Try again later!")
-          }}
-        />
+      {!error && (
+        <div align="center" style={{ marginTop: "50px" }}>
+          <img
+            src={link}
+            alt=""
+            onLoad={() => {
+              if(error) return;
+              imageResolvedRef.current = true;
+              cleanup();
+              setImageResolved(true);
+            }}
+            onError={() => {
+              imageResolvedRef.current = true;
+              cleanup();
+              statsCardMessage.current = "";
+              setError("Something went wrong loading the card. Try again later!");
+            }}
+          />
       </div>
+      )}
     </div>
   )
 }
