bca2bffd4cf7554f85bc79125396db188c0acd39
Gian Peña <flaconumero2@gmail.com>
2026-01-21 18:31:56 -0500

    make sure strict mode is off

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
   images: {
       remotePatterns: [
           new URL('https://academy.cis.fiu.edu/**'),
