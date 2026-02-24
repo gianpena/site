31556d1b380c15b0fe27d55f376e9decd9c4f043
Gian Peña <flaconumero2@gmail.com>
2026-02-24 00:10:55 -0500

    add monkeytype route

---
+++ app/api/monkeytype/route.ts
@@ -0,0 +1,18 @@
+
+export async function GET(request: Request) {
+    const mode2 = new URL(request.url).searchParams.get('mode') || '60';
+    const monkeytype = await fetch(`https://api.monkeytype.com/leaderboards/rank?language=english&mode=time&mode2=${mode2}`,
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': `ApeKey ${process.env.APE_KEY}`
+            }
+        });
+    if(!monkeytype.ok) {
+        return new Response('Failed to fetch Monkeytype leaderboard.', { status: 500 });
+    }
+
+    const monkeytypeJson = await monkeytype.json();
+    const { wpm, acc, rank } = monkeytypeJson.data;
+    return new Response(JSON.stringify({ wpm, acc, rank }));
+}
