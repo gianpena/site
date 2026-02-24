308553747a8ccb2f94fd971c43f408fb9add18e0
Gian Peña <36643266+gianpena@users.noreply.github.com>
2026-02-24 00:45:07 -0500

    next API integration (#32)
    * add monkeytype route
    
    * add typegg route
    
    * amend phrasing for consistency
    
    * remove typeracer card from speedtyping
    
    * update speedtyping page
    
    uses the local api now

--- app/speedtyping/page.tsx
+++ app/speedtyping/page.tsx
@@ -1,36 +1,28 @@
 import Card from '../card';
 import '../styles.css';
-const monkeytype_api_key = process.env.APE_KEY;
 
-async function monkeytype(mode: string, mode2: string | number) {
-    const response = await fetch(`https://api.monkeytype.com/leaderboards/rank?language=english&mode=${mode}&mode2=${mode2}`, {
-        headers: {
-            'Authorization': `ApeKey ${monkeytype_api_key}`
-        }
-    });
-
-    if(!response.ok) return {};
-    return await response.json();
-}
-
-async function typeracer() {
-    const response = await fetch('https://api.gianpena.xyz/typeracer');
-    if(!response.ok) return {};
-    return await response.json();
-}
-
-async function typegg() {
-    const response = await fetch('https://api.typegg.io/v1/users/gian');
-    if(!response.ok) return {};
-    return await response.json();
+interface TypingData {
+    wpm: number,
+    acc: number,
+    rank: number
 }
 
 export default async function SpeedTypingPage() {
 
-    const time60 = await monkeytype('time', 60);
-    const time15 = await monkeytype('time', 15);
-    const typeracer_textbests = await typeracer();
-    const typegg_data = await typegg();
+    let time60: TypingData | null = null;
+    let time15: TypingData | null = null;
+    let typegg: TypingData | null = null;
+    try {
+        const time60Response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/monkeytype?mode=60`);
+        const time15Response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/monkeytype?mode=15`);
+        const typeggResponse = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/typegg`);
+
+        time60 = await time60Response.json();
+        time15 = await time15Response.json();
+        typegg = await typeggResponse.json();
+    } catch (error) {
+        console.error('Error fetching typing data:', error);
+    }
 
     return (
         <div className="centered" style={{'gap': '20px'}}>
@@ -40,16 +32,13 @@ export default async function SpeedTypingPage() {
 
             <div className="card-container">
                 <Card title="MonkeyType time60 Personal Best">
-                    WPM: {time60?.data?.wpm.toFixed(2) ?? 'Not found. Try again later.'} {`(${time60?.data?.acc.toFixed(2)}% accuracy, rank ${time60?.data?.rank ?? 'not found'})`}
+                    WPM: {time60?.wpm.toFixed(2) ?? 'Not found. Try again later.'} {`(${time60?.acc.toFixed(2)}% accuracy, rank ${time60?.rank ?? 'not found'})`}
                 </Card>
                 <Card title="MonkeyType time15 Personal Best">
-                    WPM: {time15?.data?.wpm.toFixed(2) ?? 'Not found. Try again later.'} {`(${time15?.data?.acc.toFixed(2)}% accuracy, rank ${time15?.data?.rank ?? 'not found'})`}
-                </Card>
-                <Card title="TypeRacer Text Bests Average">
-                    WPM: {typeracer_textbests.stats}
+                    WPM: {time15?.wpm.toFixed(2) ?? 'Not found. Try again later.'} {`(${time15?.acc.toFixed(2)}% accuracy, rank ${time15?.rank ?? 'not found'})`}
                 </Card>
                 <Card title="TypeGG nWPM">
-                    nWPM: {typegg_data?.stats?.nWpm ?? 'Not found. Try again later.'} (rank {typegg_data?.globalRank ?? 'not found'})
+                    nWPM: {typegg?.wpm ?? 'Not found. Try again later.'} (rank {typegg?.rank ?? 'not found'})
                 </Card>
             </div>
         </div>
