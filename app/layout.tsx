dded9eb31019a3b18341722a5cd7d09f81bf2a58
Gian Peña <flaconumero2@gmail.com>
2026-05-12 03:42:17 -0400

    use next fonts instead of css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
+import { Noto_Serif } from "next/font/google";
 import Header from './header';
 import Footer from './footer';
 import './styles.css';
+
+const notoSerif = Noto_Serif({
+  subsets: ['latin'],
+  weight: 'variable',
+  style: ['normal', 'italic'],
+});
 
 export const metadata: Metadata = {
   title: ":3",
@@ -14,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={notoSerif.className}>
       <body style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Header />
         <main style={{ flex: '1' }}>{children}</main>
