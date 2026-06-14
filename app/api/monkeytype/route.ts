65e9eef0b44ee41ef17e74754cceca257bcd3a3f
Gian Peña <flaconumero2@gmail.com>
2026-06-13 23:28:09 -0400

    make sure mode is valid before processing

--- app/api/monkeytype/route.ts
+++ app/api/monkeytype/route.ts
@@ -1,6 +1,9 @@
 
 export async function GET(request: Request) {
     const mode2 = new URL(request.url).searchParams.get('mode') || '60';
+    if(!['60','15'].includes(mode2))
+        return Response.json({ error: 'Invalid mode. Allowed values are 60 and 15.' }, { status: 400 });
+
     const monkeytype = await fetch(`https://api.monkeytype.com/leaderboards/rank?language=english&mode=time&mode2=${mode2}`,
         {
             method: 'GET',
@@ -14,5 +17,5 @@ export async function GET(request: Request) {
 
     const monkeytypeJson = await monkeytype.json();
     const { wpm, acc, rank } = monkeytypeJson.data;
-    return new Response(JSON.stringify({ wpm, acc, rank }));
+    return Response.json({ wpm, acc, rank });
 }
